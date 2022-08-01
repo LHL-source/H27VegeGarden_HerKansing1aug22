96c9fbf43fad7711eb73072ae8b78223382ca584
LHL-source <97887598+LHL-source@users.noreply.github.com>
2022-08-01 15:30:01 +0200

    Add files via upload

--- VegeGarden/garden.js
+++ VegeGarden/garden.js
@@ -2,32 +2,56 @@
 const getYieldForPlant = (input, factor) => {
     if (!factor) {
         return input.yield
-    };
+    }; //!factor
 
-    return parseFloat(yieldInclFac.toFixed(2));
-};
-//add only the effect of sun
-let sun;
-if (!input.factors.sun) {
-    sun = 1;
-} else {
-    switch (factor.sun) {
-        case "low":
-            sun = (100 + input.factors.sun.low) / 100;
-            break;
-        case "medium":
-            sun = (100 + input.factors.sun.medium) / 100;
-            break;
-        case "high":
-            sun = (100 + input.factors.sun.high) / 100;
-            break;
-        default:
-            sun = 1;
-    } //switch
-}
+    //add only the effect of sun
+    let sun;
+    if (!input.factors.sun) {
+        sun = 1;
+    } /*!input.factors.sun*/
+    else {
+        switch (factor.sun) {
+            case "low":
+                sun = (100 + input.factors.sun.low) / 100;
+                break;
+            case "medium":
+                sun = (100 + input.factors.sun.medium) / 100;
+                break;
+            case "high":
+                sun = (100 + input.factors.sun.high) / 100;
+                break;
+            default:
+                sun = 1;
+        }; //switch
+    }; //else
 
+    //calculate the effect of only wind
+    if (!input.factors.wind) {
+        wind = 1;
+    } /*if*/
+    else {
+        switch (factor.wind) {
+            case "low":
+                wind = (100 + input.factors.wind.low) / 100;
+                break;
+            case "medium":
+                wind = (100 + input.factors.wind.medium / 100);
+                break;
+            case "high":
+                wind = (100 + input.factors.wind.high) / 100;
+                break;
+            default:
+                wind = 1;
+        }; /*switch*/
+
+    }; /*else*/
+
+    console.log("sun ,wind:", sun, wind);
+    const yieldPerPlant = input.yield * sun * wind;
+    console.log("yield per plant: ", yieldPerPlant);
+    return parseFloat(yieldPerPlant.toFixed(2));
+}; //const getYieldForPlant
 //end getYieldForPlant-- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-
 
 
 
--- VegeGarden/garden.test.js
+++ VegeGarden/garden.test.js
@@ -9,22 +9,36 @@ const {
 } = require("./Vegetable");
 //getYieldForPlant  ----------------------------------------------
 describe("getYieldForPlant No Environment factors ", () => {
-    const corn = {
-        name: "cumcumber",
-        yield: 5,
-    };
-    //test getYieldForplant no environ w? yes good job
+
+    //test getYieldForplant no environ w? yes on 1aug good job
     test("Get yield for plant with no environment factors", () => {
+
+        const cucumber = {
+            name: "cucumber",
+            yield: 5,
+        };
+
         expect(getYieldForPlant(cucumber)).toBe(5);
     }); //test
 
 
-    //test getYieldForplant with only 1 factor in this case sun high
-    test(" getYieldForplant with only 1 factor in this case sun high"), () => {
+    //test getYieldForplant with only 1 factor in this case sun high w? yes on 1 aug good job
+    test(" getYieldForplant with only 1 factor in this case sun high", () => {
         const environmentFactors = {
             sun: "high",
         };
         expect(getYieldForPlant(corn, environmentFactors)).toBe(45);
-    }
+    }); //test
+
+    //test with 2 factors: sun medium and wind high
+    test("getYieldForPlant with 2 factors sun medium and wind high", () => {
+        const environmentFactors = {
+            sun: "medium",
+            wind: "high",
+        };
+        expect(getYieldForPlant(pumpkin, environmentFactors)).toBe(1.60);
+
+    }); /*test*/
+
 }); //desc
 //end getYieldForplant--------------------------------------------
